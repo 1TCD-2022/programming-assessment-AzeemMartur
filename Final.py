e0812b57e8286b0de0b9f4497c2ce9e326bba258
AzeemMartur <103226195+AzeemMartur@users.noreply.github.com>
2022-08-24 12:44:53 +1200

    Create Final.py

---
+++ Final.py
@@ -0,0 +1,314 @@
+import time
+"""
+Filename:main.py
+Author:Abdulazeem Martur
+Date: 1/07/2022
+Description: This program is a anime quiz that can be played by anyone. There will be 10 questions and the user will choose 1,2 or 3 for there answer. At the end the socre will be printed.
+"""
+#There are 3 lists one fore questins, one for answere,and one fore the answer feed back, 
+QUESTION = [
+  "Q1 Who is The Main Character of Naruto?",
+  "Q2 Luffy's Middle Name?",
+  "Q3 What is the higest selling manga",
+  "Q4 In which anime series is Ash Ketchum the main character?",
+  "Q5 Who created Attack on Titan?",
+  "Q6 What is Toie animation's Mascot?",
+  "Q7 What type of anime is HAIKYU!!?",
+  "Q8 What are the swords called in Bleach?",
+  "Q9 What is the most in-demand show of 2021?",
+  "Q10 Which shinigami likes apples?",
+  "BonusQ1 What anime is the Character Willy Tybur from?",
+  "BonusQ2 How many filler epsiodes are in All of Naruto?",
+  "BonusQ3 Which team did Karasuno have to beat to get to the Nationals?",
+  "BonusQ4 Who is the lion Tamer in Buggy's Crew (One Piece)?",
+  "BonusQ5 Who was the second Kazekage (Naruto)?"
+  ]
+ANSWER = [
+  "1.Sasuke, 2.Nagato, 3.Naruto ",
+  "1.D, 2.A, 3.T ",
+  "1.Dragon Ball, 2.One Piece, 3.Attack on Titan ",
+  "1.Pokemon, 2.Dororo, 3.Doraemon",
+  "1.Eiichiro Oda, 2.Hajime Isayama, 3.Tite Kubo",
+  "1.Totoro, 2.Pero, 3.Bugs Bunny",
+  "1.Fighting, 2.Sports, 3.Romance",
+  "1.Zanpakutō, 2.Nichirin blade, 3.Saijō Ō Wazamono",
+  "1.Kimetsu no Yaiba 2.Boruto 3. Attack on Titan",
+  "1.Ryuk, 2.Rem 3.Mello",
+  "1.Death Note, 2.Vinland Saga, 3.Attack on Titan",
+  "1. 275, 2.312, 3.195",
+  "1.Inarizaki, 2.Aoba Johsai, 3.Shiratorizawa",
+  "1.Cabaji, 2.Mohji, 3.Galdino",
+  "1.Shamon, 2.Sandaime Kazekage, 3.Rasa"
+  
+]
+ANSWER_FEEDBACK = ["Correct","Wrong"]
+# adding points so the user know how many points they have. All the points will be printed at the end.
+points=0
+#I have wolcomed the user by asking there name which be later used to print the score
+print("Welcome to Anime Quiz")
+time.sleep(0.2)
+user_name = input("What is your name ").strip()
+print("Hello ",user_name)
+time.sleep(0.2)
+print ("The Quiz is about to start, your points will be printed at the end")
+print(" ")
+time.sleep(0.5)
+#ask the user the questions
+print (QUESTION[0])
+i=0
+#use wile loop to valadate the user option
+while i<999999999999:
+    A1 = input(ANSWER[0]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "3":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "1" or A1 == "2":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("  ")
+print (QUESTION[1])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[1]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "1":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "3" or A1 == "2":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+  
+print("  ")
+
+print (QUESTION[2])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[2]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "2":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "1" or A1 == "3":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+  
+print("  ")
+
+print (QUESTION[3])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[3]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "1":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "2" or A1 == "3":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("  ")
+
+print (QUESTION[4])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[4]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "2":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "1" or A1 == "3":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("  ")
+
+print (QUESTION[5])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[5]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "2":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "1" or A1 == "3":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("  ")
+
+print (QUESTION[6])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[6]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "2":
+        print(ANSWER_FEEDBACK[0])
+        points += 1 
+        break
+    elif A1 == "1" or A1 == "3":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("  ")
+
+print (QUESTION[7])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[7]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "1":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "2" or A1 == "3":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("  ")
+
+print (QUESTION[8])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[8]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "3":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "2" or A1 == "1":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("  ")
+
+print (QUESTION[9])
+i=0
+while i<999999999999:
+    A1 = input(ANSWER[9]+"\nChoose option 1,2 or 3 ").strip()
+    if A1 == "1":
+        print(ANSWER_FEEDBACK[0])
+        points = points + 1
+        break
+    elif A1 == "2" or A1 == "3":
+        print(ANSWER_FEEDBACK[1])
+        break
+    else:
+      print("Choose valid option") 
+    i+=1
+
+print("Calculating score.....")
+
+time.sleep(3)
+
+# the amount of points the user got will be printed here.
+print("{} got {} out of 10".format(user_name,points))
+time.sleep(0.3)
+("   ")
+if points == 10:
+  print ("You have got 10/10 you will be asked 5 bonus questions")
+  time.sleep(0.5)
+  print("  ")
+  print (QUESTION[10])
+  i=0
+  while i<999999999999:
+      A1 = input(ANSWER[10]+"\nChoose option 1,2 or 3 ").strip()
+      if A1 == "3":
+          print(ANSWER_FEEDBACK[0])
+          points = points + 1
+          break
+      elif A1 == "2" or A1 == "1":
+          print(ANSWER_FEEDBACK[1])
+          break
+      else:
+        print("Choose valid option") 
+      i+=1
+  print("  ")
+  print (QUESTION[11])
+  i=0
+  while i<999999999999:
+      A1 = input(ANSWER[11]+"\nChoose option 1,2 or 3 ").strip()
+      if A1 == "1":
+          print(ANSWER_FEEDBACK[0])
+          points = points + 1
+          break
+      elif A1 == "2" or A1 == "3":
+          print(ANSWER_FEEDBACK[1])
+          break
+      else:
+        print("Choose valid option") 
+      i+=1 
+  print("  ")
+  print (QUESTION[12])
+  i=0
+  while i<999999999999:
+      A1 = input(ANSWER[12]+"\nChoose option 1,2 or 3 ").strip()
+      if A1 == "3":
+          print(ANSWER_FEEDBACK[0])
+          points = points + 1
+          break
+      elif A1 == "2" or A1 == "1":
+          print(ANSWER_FEEDBACK[1])
+          break
+      else:
+        print("Choose valid option") 
+      i+=1
+  print("  ")
+  print (QUESTION[13])
+  i=0
+  while i<999999999999:
+      A1 = input(ANSWER[13]+"\nChoose option 1,2 or 3 ").strip()
+      if A1 == "2":
+          print(ANSWER_FEEDBACK[0])
+          points = points + 1
+          break
+      elif A1 == "3" or A1 == "1":
+          print(ANSWER_FEEDBACK[1])
+          break
+      else:
+        print("Choose valid option") 
+      i+=1
+  print("  ")
+  print (QUESTION[14])
+  i=0
+  while i<999999999999:
+      A1 = input(ANSWER[14]+"\nChoose option 1,2 or 3 ").strip()
+      if A1 == "1":
+          print(ANSWER_FEEDBACK[0])
+          points = points + 1
+          break
+      elif A1 == "2" or A1 == "3":
+          print(ANSWER_FEEDBACK[1])
+          break
+      else:
+        print("Choose valid option") 
+      i+=1
+  print("Calculating score.....")
+
+  time.sleep(3)
+  print("{} got {} out of 15".format(user_name,points))
